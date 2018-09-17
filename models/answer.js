71b87e1ab589f1e439113422a16c1a46c5033552
beavinash <beavinash15@gmail.com>
2018-09-17 19:07:50 +0000

    solved total time, response time, and questionLoadTime

--- models/answer.js
+++ models/answer.js
@@ -33,6 +33,9 @@ var respondentsSchema = mongoose.Schema({
         },
         responseTime:{
             type:String
+        },
+        questionLoadTime: {
+            type: String
         }
     }]
     
